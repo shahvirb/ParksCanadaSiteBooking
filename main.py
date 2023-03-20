eacde64821e696f6fe24670db7a7a631477cc9e1
shahvirb <shahvirb@gmail.com>
2023-03-20 17:30:14 -0500

    quick refactor so that the loop is more robust but this still is not reliable or fast (mutually exclusive currently)

--- main.py
+++ main.py
@@ -15,22 +15,39 @@ def make_browser(url: str) -> webdriver.Chrome:
     driver.get(url)
     return driver
 
-def wait_and_get_elements(driver, xpath, wait=WAIT_SEC):
+
+def wait_and_get_elements(driver, xpath, wait=WAIT_SEC, until=EC.presence_of_element_located):
     try:
-        WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        WebDriverWait(driver, wait).until(until((By.XPATH, xpath)))
         return driver.find_elements(By.XPATH, xpath)
     except selenium.common.exceptions.TimeoutException:
         return None
 
-def click_list_view(driver):
-    LIST_VIEW = r'//div[@class="btn-search-results-toggle-label"]'
-    wait_and_get_elements(driver, LIST_VIEW)[1].click()
 
-def reserve_list_item(driver, x:int):
+def click_list_view(driver):
+    VIEW_BUTTONS = r'//div[@class="btn-search-results-toggle-label"]'
+    # the list view button is the second of these
+    wait_and_get_elements(driver, VIEW_BUTTONS)[1].click()
+
+
+def expand_list_item(driver, i:int):
+    """
+    :param driver:
+    :param x: the index of the list item to click
+    :return:
+    """
     items = wait_and_get_elements(driver, r'//*[@class="resource-name"]')
-    items[x].click()
-    reserve_btn = wait_and_get_elements(driver, f'//button[@id="reserveButton-{x}"]')[0]
+    items[i].click()
+
+def click_reserve(driver, i:int):
+    """
+    :param driver:
+    :param i: the index of the list item used by expand_list_item on which to click reserve
+    :return:
+    """
+    reserve_btn = wait_and_get_elements(driver, f'//button[@id="reserveButton-{i}"]')[0]
     reserve_btn.click()
+
 
 if __name__ == '__main__':
     driver = make_browser(START_URL)
@@ -39,14 +56,22 @@ if __name__ == '__main__':
     # driver.switch_to.window(driver.window_handles[1])
     # driver.get("https://google.com")
 
+    SITE_TO_RESERVE = 0
+
     click_list_view(driver)
-    # click the first resource
+    expand_list_item(driver, SITE_TO_RESERVE)
     while True:
-        reserve_list_item(driver, 1)
-        if wait_and_get_elements(driver, r'//app-reserve-restriction-dialog', 1):
-            close = driver.find_element(By.XPATH, r'//app-reserve-restriction-dialog//button')
-            close.click()
-            print('RESTRICTED')
+        click_reserve(driver, SITE_TO_RESERVE)
+        if wait_and_get_elements(driver, r'//app-reserve-restriction-dialog', 3, until=EC.visibility_of_element_located):
+            #close = driver.find_element(By.XPATH, r'//app-reserve-restriction-dialog//button')
+            close = wait_and_get_elements(driver, r'//app-reserve-restriction-dialog//button', 10, until=EC.element_to_be_clickable)
+            if close:
+                close[0].click()
+                print('RESTRICTED')
+            else:
+                print('Did not find close button but found app-reserve-restriction-dialog')
+
+    # TODO should we catch and ignore exceptions so they don't kill the browser if something goes wrong?
+    # TODO can we detach the browser from python before exiting so that if python crashes the browser is still intact in case the user has reserved the site and is now entering in reservation info?
 
     input("Press any key to exit")
-
